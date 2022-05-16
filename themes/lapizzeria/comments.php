d4ea0c9a7338f95236a93bea1ee87653eea6b207
Jesús Mares <maresdf@comunidad.unam.mx>
2022-05-16 12:46:40 -0500

    bloques gutenberg

--- themes/lapizzeria/comments.php
+++ themes/lapizzeria/comments.php
@@ -2,18 +2,8 @@
   <?php 
     //Comentarios
     $args = array(
-      'class_submit' => 'boton boton-primario',
-      'title_reply' => 'Deja un comentario',
-      'comment_notes_after' => '',
-      'comment_field' => '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><br /><textarea id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>',
-      'comment_notes_before' => '',
-      'comment_notes_after' => '',
-      'label_submit' => 'Publicar comentario',
-      'fields' => array(
-        'author' => '<p class="comment-form-author"><label for="author">' . __( 'Name' ) . '</label><br /><input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30" /></p>',
-        'email' => '<p class="comment-form-email"><label for="email">' . __( 'Email' ) . '</label><br /><input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30" /></p>',
-        'url' => '<p class="comment-form-url"><label for="url">' . __( 'Website' ) . '</label><br /><input id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="30" /></p>'
-      )
+      'class_submit' => 'boton boton-primario'
+      
     );
       comment_form($args); 
   ?>
